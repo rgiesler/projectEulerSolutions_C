98fb7714891edd1ddba7a26ac88076ac19e4aaf3
Rudi Giesler <rgiesler@gmail.com>
2014-06-16 15:43:17 +0200

    Problem 1 solved

--- problem1.c
+++ problem1.c
@@ -1,4 +1,16 @@
-int main()
+#include <stdio.h>
+
+int main(int args, char *argv[])
 {
+	int i = 3;
+	int sum = 0;
+
+	for(i = 3; i < 1000; i++) {
+		if (i%3==0 || i%5==0)
+			sum += i;
+	}
+
+	printf("Sum: %d\n", sum);
+
 	return 0;
 }
