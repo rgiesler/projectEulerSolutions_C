f5aeb2415f087ae9f00cb7a20c1207a2f2b2c23f
Rudi Giesler <rgiesler@gmail.com>
2014-06-17 13:36:01 +0200

    Improved problem 5 solution

--- problem5.c
+++ problem5.c
@@ -7,24 +7,47 @@
 	numbers from 1 to 20?
 */
 
-int isDivisible(const int n, int limit);
-
+/*	See theoretical description below */
 int main (int argc, char *argv[])
 {
-	// We know that we need to start at 2520, and go up in multiples of twenty
-	// since 2520 is divisible by 20
-	int i=2520;
+	int primes[8] = {2, 3, 5, 7, 11, 13, 17, 19};
+	int result = 1, i, max, max_old;
 
-	while(!isDivisible(i,20)) {i+=20;}
-
-	printf("Smallest divisible number: %d\n", i);
+	for(i = 0; i < 8; i++) {
+		max = max_old = 1;
+		while((max *= primes[i]) < 20)
+			max_old = max;
+		result *= max_old;
+	}
+	
+	printf("Smallest divisible number: %d\n", result);
 	return 0;
 }
 
-int isDivisible(const int n, int limit)
-{
-	int i;
-	for(i = 2; i <= limit; i++)
-		if(n%i != 0) return 0;
-	return 1;
-}
+
+
+/*	We can solve this problem by finding the prime factors of the numbers,
+	so that:
+
+		N = p1^a1 + p2^a2 + ... + pn^an
+
+	where p are the prime numbers, and a how many of each number we need to
+	find our final number N.
+
+	This alone, however, is not enough, as N needs to fulfill a specific 
+	criteria. We can do this by finding the powers of the primes for each 
+	divisor (from 1 to 20), and using the maximum number for a out of all of the
+	divisors. This will give us the prime factors of the number divisible from
+	1 to 20, and we can multiply those prime factors to get the answer.
+
+	We can go further though. For each a, the value of a is the biggest a so 
+	that:
+
+		pn^an <= k
+
+	where k is the maximum number. I'm not sure how to explain it, it seems 
+	logical to me, I'll show with the example of 2. 2^4 = 16 is the highest
+	possible power below 20, which means that 16 is the number which needs the 
+	most 2's, any other number needs less 2's, so the maximum power of 2 that
+	we need is 4.
+*/
